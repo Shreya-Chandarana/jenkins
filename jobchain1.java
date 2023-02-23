673e980b4c23809502a31f189ac8680a69daadd0
shreya <shreyaa.chandarana@gmail.com>
2023-02-23 16:17:33 +0530

    commiting files

---
+++ jobchain1.java
@@ -0,0 +1,6 @@
+public class jobchain1 {
+    public static void main(String[] args) { 
+	System.out.println("Hello, SHREYA!, this is my parent jobchain file");
+	
+    }
+}
---
+++ jobchain2.java
@@ -0,0 +1,6 @@
+public class jobchain2 {
+    public static void main(String[] args) { 
+	System.out.println(" this is my child jobchain file");
+	
+    }
+}
